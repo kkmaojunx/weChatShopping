a497edbd9e758f56bb653e9aa1543efef647e8be
yangjialing <1243244668@qq.com>
2018-04-11 21:33:23 +0800

    bbb

--- pages/threePages/shoppingPage/shoppingPage.js
+++ pages/threePages/shoppingPage/shoppingPage.js
@@ -12,8 +12,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url: 'shoppingPage.json', //仅为示例，并非真实的接口地址
+      data: {
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
